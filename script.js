138126fd4d0ca3698f1b55d5201885f59c6b9f6b
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-19 16:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function firstWord(s) {
-  // your code here
+  //SGN your code here
+
+let fw = '';
+	s = s.trim();
+	
+		if(s.charAt(0) == " " || s.length < 1){
+			fw = '';
+		}else{
+			for (let i = 0; i < s.length; i++) {
+				if(s.charAt(i)== " "){
+					fw = s.substring(0, i);
+					break;
+				}else {
+					fw = s;
+				}
+			}
+		}
+	
+	return fw;
 }
 
 // Do not change the code below
+
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
